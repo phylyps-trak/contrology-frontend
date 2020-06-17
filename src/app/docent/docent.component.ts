25ab72a82df916553ee17aa2d80468ac7590d9a0
Michiel <j.michiel.janssens@gmail.com>
2020-06-17 13:39:37 +0200

    bootstrap dr in

--- src/app/docent/docent.component.ts
+++ src/app/docent/docent.component.ts
@@ -2,13 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-docent',
-  templateUrl: './docent.component.html',
-  styleUrls: ['./docent.component.css']
+  templateUrl: './docent.component.html'
+  //styleUrls: ['./docent.component.css']
 })
 export class DocentComponent implements OnInit {
+// ***** 
+//even een basic functionaliteits testje maken: primitive two-way binding
+private _michielsVar:string = "deze test werkt!"
+  
+get michielsVar():string{
+  return this._michielsVar
+}
+set michielsVar(s:string){
+  this._michielsVar = s;
+}
 
+changeVar($event){
+  this.michielsVar = $event.target.value;
+}
+//  *****
+private email:string = "deze";
+
+getEmail(){
+  return this.email;
+}
+changeEmail($event){
+  this.email = $event.target.value;
+}
+//  *****
+// real two-way binding
+numbervar:number;
   constructor() { }
-
   ngOnInit(): void {
   }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,32 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-// ***** 
-//even een basic functionaliteits testje maken: primitive two-way binding
-private _michielsVar:string = "deze test werkt!"
-  
-get michielsVar():string{
-  return this._michielsVar
-}
-set michielsVar(s:string){
-  this._michielsVar = s;
-}
-
-changeVar($event){
-  this.michielsVar = $event.target.value;
-}
-//  *****
-private email:string = "deze";
-
-getEmail(){
-  return this.email;
-}
-changeEmail($event){
-  this.email = $event.target.value;
-}
-//  *****
-// real two-way binding
-numbervar:number;
   constructor() { }
 
   ngOnInit(): void {
